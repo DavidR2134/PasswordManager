2395de4e04cd1400a4046dafb0c16adc90612ecc
David Rogers <davidr@pop-os.lan>
2024-11-20 12:22:37 -0700

    store encrypted passwords and retrieve/decrypt based on user ID

--- main.py
+++ main.py
@@ -2,16 +2,35 @@ from user import User
 import sqlite3
 import getpass
 import os
-#import pyfiglet
+import pyfiglet
+from cryptography.fernet import Fernet
+import base64
+from datetime import datetime
 
+db = "passwords.db"
+
+def create_database(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()    
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL);")
+    cur.execute("CREATE TABLE IF NOT EXISTS password(id INTEGER PRIMARY KEY AUTOINCREMENT, company_name TEXT NOT NULL, password BLOB NOT NULL, last_updated TEXT NOT NULL, userID INTEGER, CONSTRAINT fk_users FOREIGN KEY (userID) REFERENCES users(id));")
+    conn.commit()
+    conn.close()
+
+    print("DB Created.")
 
 def create_user(user):
     conn = sqlite3.connect("passwords.db")
     cur = conn.cursor()
 
     try:
-        cur.execute(f"INSERT INTO users(username, password) values ('{user.username}', '{user.password}');")
-        conn.commit()
+        data = cur.execute(f"SELECT * FROM users WHERE username='{user.username}';")
+        print(len(data.fetchall()))
+        if len(data.fetchall()) == 0:
+            cur.execute(f"INSERT INTO users(username, password) values ('{user.username}', '{user.password}');")
+            conn.commit()
+        else:
+            print("Username is taken.")
     except Exception as e:
         print(f"Error: {e}")
     
@@ -31,11 +50,11 @@ def login():
         return False
 
 def clear():
-    os.system('clear')
+    #os.system('clear')
+    pass
 
 def print_menu(isLoggedIn):
-    #t = pyfiglet.figlet_format("Password Manager")
-    t = "Password Manager"
+    t = pyfiglet.figlet_format("Password Manager")
     clear()
     if isLoggedIn == False:
         print(t)
@@ -51,6 +70,53 @@ def print_menu(isLoggedIn):
         print("3) Update Password")
         print("4) Delete Password")
         print("5) Logout")
+
+def fetch_passwords(user):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    try:
+        data = cur.execute(f"SELECT company_name as company, password, last_updated FROM password WHERE userID={user._id}")
+        data = data.fetchall()
+
+        show_passwords(data)
+        conn.close()
+    except Exception as e:
+        print(e)
+
+def show_passwords(data):
+    key = user.generate_key()
+    cipher = Fernet(key)
+
+    print("+" + "-" * len(f"| {data[0][0]} | {cipher.decrypt(data[0][1]).decode()} | {data[0][2]} |") + "+")
+    for i in range(len(data)):
+        print(f"| {data[i][0]} | {cipher.decrypt(data[i][1]).decode()} | {data[i][2]} |" )
+    print("+" + "-" * len(f"| {data[0][0]} | {cipher.decrypt(data[0][1]).decode()} | {data[0][2]} |") + "+")
+
+def add_password(user):
+    key = user.generate_key()
+    print(key)
+    cipher = Fernet(key)
+    company_name = input("Please enter a company: ")
+    passwd = cipher.encrypt(getpass.getpass("Please enter the password: ").encode())
+
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"""INSERT INTO password(company_name, password, last_updated, userID) 
+                    VALUES(?,?,?,?);""", (company_name, passwd, datetime.now(), user._id))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(e)
+        conn.close()    
+
+try:
+    test = open("passwords.db", 'rb')
+except:
+    create_database(db)
+
 
 isLoggedIn = False
 while True:
@@ -70,6 +136,7 @@ while True:
             if p == p2:
                 user = User(u, p)
                 create_user(user)
+                input()
             else:
                 print("Passwords do not match.")
                 input("Press enter to continue...")
@@ -78,5 +145,10 @@ while True:
             print("Quitting...")
             break
     else:
-        print("Logged in")
-
+        if choice == 1:
+            fetch_passwords(user)
+        elif choice == 2:
+            print(user.password)
+            add_password(user)
+        elif choice == 5:
+            isLoggedIn = False
--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 import hashlib
 import sqlite3
+import base64
 
 class User:
     def __init__(self, username, password):
@@ -16,7 +17,6 @@ class User:
             u = cur.execute(f"SELECT * FROM users where username='{self.username}';")
             u = u.fetchone()
             conn.close()
-            print(f"u0 = {u[0]}\nu1 = {u[1]}\nu2 = {u[2]}\n")
             if self.password == u[2]:
                 return u[0]
             else:
@@ -24,6 +24,21 @@ class User:
         except Exception as e:
             conn.close()
             return -777
+
+    def generate_key(self):
+        if len(self.password) % 2 == 0 and all(c in "0123456789abcdefABCDEF" for c in self.password):
+            key_bytes = bytes.fromhex(self.password)
+            base64_key = base64.urlsafe_b64encode(key_bytes)
+        else:
+            base64_key = self.password.encode("utf-8")
+        
+        padding_needed = len(base64_key) % 4
+        if padding_needed != 0:
+            base64_key += b"=" * (4-padding_needed)
+
+        return base64_key
+
+
 
     def __str__(self):
         return f"{self.username}: hashed_password -> {self.password}"
